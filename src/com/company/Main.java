2da81f2755e15bccfa8bb2907ab6ac62a1e8d2c5
N1KF0X <96484000+N1KF0X@users.noreply.github.com>
2022-10-03 12:04:00 +0300

    init commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,28 @@
 package com.company;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws MalformedURLException {
-	    MusicDownloader musicDownloader = new MusicDownloader("https://ru.hitmotop.com/song/56774126");
-        musicDownloader.start();
+        Scanner scanner = new Scanner(System.in);
+        String line;
+
+        System.out.println("Вводите ссылки для скачивания музыки по очереди. Для окончания процедуры введите \"End\"");
+
+        try {
+            while (true) {
+                line = scanner.nextLine();
+                if(line.equals("End")){
+                    break;
+                }
+                new MusicDownloader(line).start();
+            }
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+            System.out.println("Введены некоректные данные");
+        }
     }
 }
--- src/com/company/MusicDownloader.java
+++ src/com/company/MusicDownloader.java
@@ -13,8 +13,9 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class MusicDownloader extends Thread{
+    private final static String REGULAR = "https:\\/\\/ru.hitmotop.com\\/get\\/music(.+).mp3";
+    private final static String PATH = "D:\\Music\\music.mp3";
     URL pageUrl;
-    URL musicUrl;
     String page;
 
     public MusicDownloader(String url) throws MalformedURLException {
@@ -26,31 +27,20 @@ public class MusicDownloader extends Thread{
         super.run();
         try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(pageUrl.openStream()))){
             page = bufferedReader.lines().collect(Collectors.joining("\n"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Pattern email_pattern = Pattern.compile("https:\\/\\/ru.hitmotop.com\\/get\\/music(.+).mp3");
-        Matcher matcher = email_pattern.matcher(page);
-        matcher.find();
-        try {
-            musicUrl = new URL(matcher.group());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        System.out.println(matcher.group());
-        try {
-            downloadUsingNIO(matcher.group(), "D:\\Music\\music.mp3");
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            Pattern email_pattern = Pattern.compile(REGULAR);
+            Matcher matcher = email_pattern.matcher(page);
+            matcher.find();
+
+            ReadableByteChannel byteChannel = Channels.newChannel(new URL(matcher.group()).openStream());
+            FileOutputStream stream = new FileOutputStream(PATH);
+            stream.getChannel().transferFrom(byteChannel, 0, Long.MAX_VALUE);
+
+            stream.close();
+            byteChannel.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
-    private static void downloadUsingNIO(String strUrl, String file) throws IOException {
-        URL url = new URL(strUrl);
-        ReadableByteChannel byteChannel = Channels.newChannel(url.openStream()); // Создает байтовый канал для чтения сайта
-        FileOutputStream stream = new FileOutputStream(file); // Создает поток для записи
-        stream.getChannel().transferFrom(byteChannel, 0, Long.MAX_VALUE);//Записывает в поток данные
-        stream.close();
-        byteChannel.close();
-    }
 }
